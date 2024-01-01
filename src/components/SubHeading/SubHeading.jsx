783b20763eb51e9c3be371868a06a1e0eea75048
Abiola Olamilekan <abiolawalter2015@gmail.com>
2024-01-01 09:26:39 +0100

    feat: implement header section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,5 +1,12 @@
-const SubHeading = () => {
-  return <div>SubHeading</div>;
+import { spoon } from "../../constants/images";
+
+const SubHeading = ({ title }) => {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <p className="p__cormorant">{title}</p>
+      <img src={spoon} alt="spoon" className="spoon__img" />
+    </div>
+  );
 };
 
 export default SubHeading;
