2a21aba657103a2effb78dd66f03b0a77f45d9f4
Abiola Olamilekan <abiolawalter2015@gmail.com>
2024-01-03 07:38:30 +0100

    feat: implement awards section

--- src/modules/Laurels/Laurels.jsx
+++ src/modules/Laurels/Laurels.jsx
@@ -1,5 +1,42 @@
 import "./Laurels.css";
 
-const Laurels = () => <div>Laurels</div>;
+import { SubHeading } from "../../components";
+import { awards } from "../../constants/data";
+import { laurels } from "../../constants/images";
+
+const Laurels = () => (
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {awards.map(({ imgUrl, title, subtitle }, idx) => (
+          <AwardCard
+            key={idx}
+            imgUrl={imgUrl}
+            title={title}
+            subtitle={subtitle}
+          />
+        ))}
+      </div>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={laurels} alt="laurels" />
+    </div>
+  </div>
+);
 
 export default Laurels;
+
+const AwardCard = ({ imgUrl, title, subtitle }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="award" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
+  </div>
+);
